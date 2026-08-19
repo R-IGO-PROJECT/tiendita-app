71af528f37e7d35a2a63a55cab1ed0776d740ba3
R-IGO-PROJECT <pruebadeproyectos1@gmail.com>
2026-08-18 19:51:30 -0600

    feat/agregar funciones de registro y lectura
    Se agregaron las funciones que son para registrar datos en la base de datos, agregar_categoria que es para registrar una categoria, y agregar_producto que es para registrar un producto, y la funcion de lectura que es obtener_productos que es para ver todos los productos de la base de datos

--- base_datos.py
+++ base_datos.py
@@ -33,6 +33,51 @@ def inicializar_base_datos():
     conexion.close()
     print("¡Base de datos y tablas creadas con éxito!")
 
+def agregar_categoria(nombre):
+    """Inserta una nueva categoría utilizando consultas parametrizadas (?)"""
+    conexion = sqlite3.connect("tiendita.db")
+    cursor = conexion.cursor()
+    try:
+        cursor.execute("INSERT INTO categorias (nombre) VALUES (?)", (nombre,))
+        conexion.commit()
+        print(f"Categoría '{nombre}' agregada con éxito.")
+    except sqlite3.IntegrityError:
+        print(f"Error: La categoría '{nombre}' ya existe.")
+    finally:
+        conexion.close()
+
+def agregar_producto(nombre, precio, stock, categoria_id):
+    """Inserta un nuevo producto en el inventario"""
+    conexion = sqlite3.connect("tiendita.db")
+    cursor = conexion.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON;")
+    try:
+        cursor.execute("""
+            INSERT INTO productos (nombre, precio, stock, categoria_id)
+            VALUES (?, ?, ?, ?)
+        """, (nombre, precio, stock, categoria_id))
+        conexion.commit()
+        print(f"Producto '{nombre}' agregado correctamente.")
+    except sqlite3.IntegrityError:
+        print("Error: La categoría asignada no existe.")
+    finally:
+        conexion.close()
+
+def obtener_productos():
+    """Devuelve la lista de productos unida a sus nombres de categoría (JOIN)"""
+    conexion = sqlite3.connect("tiendita.db")
+    cursor = conexion.cursor()
+    
+    # Hacemos un JOIN para traer el nombre de la categoría en lugar de solo su ID
+    cursor.execute("""
+        SELECT p.id, p.nombre, p.precio, p.stock, c.nombre 
+        FROM productos p
+        LEFT JOIN categorias c ON p.categoria_id = c.id
+    """)
+    productos = cursor.fetchall()
+    conexion.close()
+    return productos
+
 # Permite ejecutar la función directamente desde la terminal
 if __name__ == "__main__":
     inicializar_base_datos()
